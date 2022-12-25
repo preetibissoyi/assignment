67fadc9391da8446f14c32a26bab120918d9def9
Jyotibissoyi <jyotibissoyikhusbu2571@gmail.com>
2022-12-25 20:13:48 +0530

    done

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ app.use(express.json())
 
 mongoose.set('strictQuery', true)
 
-mongoose.connect('mongodb+srv://nnsahu2022:Sahurk012@mycluster.ne522qz.mongodb.net/customerProject', { useNewUrlParser: true })
+mongoose.connect('mongodb+srv://Jyoticoder:Jyoti2025@jyoticoder-cluster.ljxxb2x.mongodb.net/coder', { useNewUrlParser: true })
     .then(() => { console.log('MongoDB is connected') })
     .catch((error) => { console.log(error) })
 
